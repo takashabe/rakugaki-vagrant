b1ba7cf955f08e719cc008c1ecac6fe147feb59f
takashabe <takashabe.public@gmail.com>
2014-02-16 01:18:51 +0900

    Add vagrant setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
 
 bundle install --path vendor/install
+
 cd chef-repo
 bundle exec berks install --path cookbooks
+
+cd ..
+vagrant up
+vagrant ssh-config --host rakugaki >> ~/.ssh/config
+
+cd chef-repo
+bundle exec knife solo cook rakugaki
