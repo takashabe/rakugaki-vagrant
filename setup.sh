187de1ed9b298df1a97adf8cf91c4cb5b7522f04
takashabe <takashabe.public@gmail.com>
2014-02-16 10:54:50 +0900

    Add knife setting

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
 
+# Setup use bundle library
 bundle install --path vendor/install
-
 cd chef-repo
 bundle exec berks install --path cookbooks
 
+# Setup vagrant
 cd ..
 vagrant up
 vagrant ssh-config --host rakugaki >> ~/.ssh/config
 
+# Setup chef-solo
 cd chef-repo
+bundle exec knife solo prepare rakugaki
 bundle exec knife solo cook rakugaki
