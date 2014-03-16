2feba89cc336bc1945aeaa6dffa20d6c9fe289b5
takashabe <takashabe.public@gmail.com>
2014-03-16 22:46:12 +0900

    Add oh-my-zsh recipe

---
+++ chef-repo/site-cookbooks/oh-my-zsh/recipes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: oh-my-zsh
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "zsh" do
+  action :install
+end
+
+git "home/rakugaki/.oh-my-zsh" do
+  repository "git://github.com/robbyrussell/oh-my-zsh.git"
+  reference  "master"
+  action     "checkout"
+  user       "rakugaki"
+  group      "rakugaki"
+end
+
+template "/home/rakugaki/.zshrc" do
+  owner "rakugaki"
+  group "rakugaki"
+  mode  "0755"
+end
